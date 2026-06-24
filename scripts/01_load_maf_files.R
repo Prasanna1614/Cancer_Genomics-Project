70f9893ded3338e2dcc508b353d219a97ac6dd0e
Prasanna Kondapalli <knlprasanna15@gmail.com>
2026-06-24 19:50:35 +0530

    load_maf_files.R

--- scripts/01_load_maf_files.R
+++ scripts/01_load_maf_files.R
@@ -1,14 +1,11 @@
 library(maftools)
-
 luad <- read.maf("LUAD.maf")
 coad <- read.maf("COAD.maf")
 
 Quick Summary
-
 getSampleSummary(luad)
 getSampleSummary(coad)
 
 Gene Summary
-
 getGeneSummary(luad)
 getGeneSummary(coad)
