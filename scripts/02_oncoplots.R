da82b8f7720e96bbf1753b94257a43d4af035c8f
Prasanna Kondapalli <knlprasanna15@gmail.com>
2026-06-24 17:33:11 +0530

    Create 02_oncoplots.R
    Generate oncoplots for LUAD and COAD

---
+++ scripts/02_oncoplots.R
@@ -0,0 +1,19 @@
+library(maftools)
+
+# LUAD oncoplot
+oncoplot(
+    maf = luad,
+    top = 10,
+    titleText = "LUAD Oncoplot"
+)
+
+# COAD oncoplot
+oncoplot(
+    maf = coad,
+    top = 10,
+    titleText = "COAD Oncoplot"
+)
+
+# Mutation summaries
+plotmafSummary(maf = luad)
+plotmafSummary(maf = coad)
