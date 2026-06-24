88b6440e381690d9ad62763fae856ea532093c9e
Prasanna Kondapalli <knlprasanna15@gmail.com>
2026-06-24 17:48:43 +0530

    02_oncoplots.R

--- scripts/02_oncoplots.R
+++ scripts/02_oncoplots.R
@@ -6,14 +6,12 @@ oncoplot(
     top = 10,
     titleText = "LUAD Oncoplot"
 )
-
 # COAD oncoplot
 oncoplot(
     maf = coad,
     top = 10,
     titleText = "COAD Oncoplot"
 )
-
 # Mutation summaries
 plotmafSummary(maf = luad)
 plotmafSummary(maf = coad)
