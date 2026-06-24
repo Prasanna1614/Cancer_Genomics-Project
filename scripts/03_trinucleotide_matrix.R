004b50bfa204fda99f7a81a22a0c66e5e1b75df7
Prasanna Kondapalli <knlprasanna15@gmail.com>
2026-06-24 17:46:10 +0530

    03_trinucleotide_matrix.R
    matrix  generation and ref genome comaparision

---
+++ scripts/03_trinucleotide_matrix.R
@@ -0,0 +1,14 @@
+library(MutationalPatterns)
+library(BSgenome.Hsapiens.UCSC.hg38)-ref genome
+
+# LUAD mutation matrix
+luad_tally <- trinucleotideMatrix(
+  maf = luad,
+  ref_genome = "BSgenome.Hsapiens.UCSC.hg38"
+)
+
+# COAD mutation matrix
+coad_tally <- trinucleotideMatrix(
+  maf = coad,
+  ref_genome = "BSgenome.Hsapiens.UCSC.hg38"
+)
